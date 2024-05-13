77a5eece3d9699aa95df18439f47d3cde94c52c3
Lada <lada11lada@gmail.com>
2024-05-13 20:55:26 +0200

    HW-07: task-1

---
+++ 07/js/task-1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const categories = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categories.length}`);
+
+for (const category of categories) {    
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.children.length}`);
+}
